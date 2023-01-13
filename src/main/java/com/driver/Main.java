fee52cbb056ac7de95117d31f3e59027be5d15d7
gori39 <shakeels1939@gmail.com>
2023-01-13 13:13:40 +0530

    poly update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,27 @@
 package com.driver;
 import java.util.*;
 public class Main {
+
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
-       Product p = new Product();
-       p.product(15,20);
-       p.product(10,20,30);
-       p.product(8.5,9.5);
+        Product p = new Product();
+        p.product(15, 20);
+        p.product(10, 20, 30);
+        p.product(8.5, 9.5);
     }
 
-}
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
 }
